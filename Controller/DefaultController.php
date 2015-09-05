acf9ce138283d5e323c43aa9638851033130f7ef
Robert M. Parker <rparker@yamiko.ninja>
2015-09-05 00:36:51 -0700

    set up code sniffing

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -4,8 +4,20 @@ namespace Yamiko\CacheAdministrationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+/**
+ * Default controller.
+ *
+ * Class DefaultController
+ */
 class DefaultController extends Controller
 {
+    /**
+     * index action.
+     *
+     * @param string $name name of person to say hello to
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction($name)
     {
         return $this->render('YamikoCacheAdministrationBundle:Default:index.html.twig', array('name' => $name));
